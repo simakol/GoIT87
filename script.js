a2a6639cfd8c38ac0ac202a8427341878212a1b9
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-08-01 21:43:25 +0300

    lesson-17

--- script.js
+++ script.js
@@ -61,45 +61,75 @@
 // console.log('3');
 
 // ***********Class Date*********** \\
-const currentDate = new Date();
-const targetDate = new Date("Sep 05 2023");
+// const currentDate = new Date();
+// const targetDate = new Date("Sep 05 2023");
 
-console.log(targetDate - currentDate);
+// console.log(targetDate - currentDate);
 
 // Практика
 // Потрібно створити два приклади годинника (Електронний та механічний)
 
-// const refs = {
-//   day: document.querySelector(".date-day"),
-//   date: document.querySelector(".date"),
-//   month: document.querySelector(".date-month"),
-//   year: document.querySelector(".date-year"),
-//   clock: document.querySelector(".digital-clock"),
-//   seconds: document.querySelector(".clock-seconds__arrow"),
-//   minutes: document.querySelector(".clock-minutes__arrow"),
-//   hours: document.querySelector(".clock-hours__arrow"),
-// };
+const refs = {
+  day: document.querySelector(".date-day"),
+  date: document.querySelector(".date"),
+  month: document.querySelector(".date-month"),
+  year: document.querySelector(".date-year"),
+  clock: document.querySelector(".digital-clock"),
+  seconds: document.querySelector(".clock-seconds__arrow"),
+  minutes: document.querySelector(".clock-minutes__arrow"),
+  hours: document.querySelector(".clock-hours__arrow"),
+};
 
-// const arrDay = [
-//   "Неділя",
-//   "Понеділок",
-//   "Вівторок",
-//   "Середа",
-//   "Четвер",
-//   "П`ятниця",
-//   "Субота",
-// ];
-// const namesOfMonth = [
-//   "Січень",
-//   "Лютий",
-//   "Березень",
-//   "Квітень",
-//   "Травень",
-//   "Червень",
-//   "Липень",
-//   "Серпень",
-//   "Вересень",
-//   "Жовтень",
-//   "Листопад",
-//   "Грудень",
-// ];
+const arrDay = [
+  "Неділя",
+  "Понеділок",
+  "Вівторок",
+  "Середа",
+  "Четвер",
+  "П`ятниця",
+  "Субота",
+];
+
+const namesOfMonth = [
+  "Січень",
+  "Лютий",
+  "Березень",
+  "Квітень",
+  "Травень",
+  "Червень",
+  "Липень",
+  "Серпень",
+  "Вересень",
+  "Жовтень",
+  "Листопад",
+  "Грудень",
+];
+
+setInterval(() => {
+  const currentDate = new Date();
+
+  const day = currentDate.getDay();
+  const date = currentDate.getDate();
+  const month = currentDate.getMonth();
+  const year = currentDate.getFullYear();
+
+  const time = currentDate.toLocaleTimeString("uk-UA");
+
+  refs.day.textContent = arrDay[day];
+  refs.date.textContent = date;
+  refs.month.textContent = namesOfMonth[month];
+  refs.year.textContent = year;
+
+  refs.clock.textContent = `Поточний час: ${time}`;
+
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  const seconds = currentDate.getSeconds();
+  const secondsDeg = (360 / 60) * seconds;
+  const minutesDeg = (360 / 60) * minutes;
+  const hoursDeg = (360 / 12) * hours + (360 / 12 / 60) * minutes;
+
+  refs.seconds.style.transform = `rotate(${secondsDeg}deg)`;
+  refs.minutes.style.transform = `rotate(${minutesDeg}deg)`;
+  refs.hours.style.transform = `rotate(${hoursDeg}deg)`;
+}, 1000);
