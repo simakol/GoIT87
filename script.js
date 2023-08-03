30e780ccde965edcaab19bcd5c6ae05f652608e6
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-08-03 22:03:12 +0300

    lesson-18

--- script.js
+++ script.js
@@ -75,6 +75,7 @@
 //   .finally(() => console.log("FINALLY!"));
 
 // ****************************Практика************************* \\
+
 // Наше завдання написати програмне забезпечення для ігрового автомата
 // Для вирішення завдання використай готову розмітку HTML та базову стилізацію
 // Після натиснення на кнопку "Start game" в кожному віконці по черзі має з'являтись смайлик з затримкою в 1 секунду ('🤑' або '👿')
@@ -83,3 +84,48 @@
 // Виводить модальне вікно з повідомленням про статус гри ('Winner' або 'Loser')
 // Для модального вікна використовуй бібліотеку basicLightbox
 // Після повторного натискання на кнопку "Start game" поле має очищатись, а гра починатись з початку.
+
+const refs = {
+  start: document.getElementById("start"),
+  container: document.getElementById("container"),
+};
+
+refs.start.addEventListener("click", startGame);
+
+function startGame() {
+  const promises = [...refs.container.children].map(() => createPromise());
+
+  Promise.allSettled(promises).then((items) => {
+    const isWinner =
+      items.every((item) => item.status === "fulfilled") ||
+      items.every((item) => item.status === "rejected");
+
+    items.forEach((item, i) => {
+      console.log(item);
+      refs.container.children[i].textContent = "";
+
+      setTimeout(() => {
+        refs.container.children[i].textContent = item.value || item.reason;
+
+        if (i === items.length - 1) {
+          const instance = basicLightbox.create(
+            `<h1>${isWinner ? "Winner" : "Loser"}</h1>`
+          );
+          instance.show();
+        }
+      }, 1000 * (i + 1));
+    });
+  });
+}
+
+function createPromise() {
+  return new Promise((res, rej) => {
+    const rand = Math.random();
+
+    if (rand > 0.5) {
+      res("🤑");
+    } else {
+      rej("👿");
+    }
+  });
+}
