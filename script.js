087f9ec13bc0530d0567ca3aeddcec3c981e0fdd
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-08-17 22:37:47 +0300

    lesson-22

--- script.js
+++ script.js
@@ -46,7 +46,7 @@
 
 //! ASYNC AXIOS
 // async function serviceCountry() {
-//   const URL = "https://restcountries.com/v3.1/name/Ukraine5";
+//   const URL = "https://restcountries.com/v3.1/name/Ukraine";
 //   try {
 //     const { data } = await axios.get(URL);
 //     console.log(data);
@@ -68,8 +68,30 @@
 */
 
 // *********** Послідовні *********** \\
+// async function serviceCountry() {
+//   const resp1 = await axios("https://restcountries.com/v3.1/name/Ukraine");
+//   const resp2 = await axios("https://restcountries.com/v3.1/name/France");
+//   const resp3 = await axios("https://restcountries.com/v3.1/name/Poland");
+//   console.log(resp3.data);
+//   console.log(resp2.data);
+//   console.log(resp1.data);
+// }
+
+// serviceCountry();
 
 // *********** Паралельні *********** \\
+
+// async function serviceCountry() {
+//   const countries = ["Ukraine", "France", "Poland"];
+//   const resp = countries.map(async (country) => {
+//     const resp = await axios(`https://restcountries.com/v3.1/name/${country}`);
+//     console.log(resp);
+//     return resp;
+//   });
+//   console.log(resp);
+// }
+
+// serviceCountry();
 
 // ********************* Практика ********************* \\
 // Створи додаток для туристичного агенства
@@ -80,3 +102,107 @@
 // Використовуй паралельні запити
 // Бекенд для пошуку країн https://restcountries.com/
 // Бекенд для прогнозу погоди  https://www.weatherapi.com/docs/   КЛЮЧ ВАЛІДНИЙ 21 ДЕНЬ !!!!!
+
+const refs = {
+  form: document.querySelector(".js-search"),
+  formContainer: document.querySelector(".js-form-container"),
+  addField: document.querySelector(".js-add"),
+  list: document.querySelector(".js-list"),
+};
+
+refs.addField.addEventListener("click", addField);
+refs.form.addEventListener("submit", handleSearch);
+
+function addField() {
+  refs.formContainer.insertAdjacentHTML(
+    "beforeend",
+    '<input type="text" name="country" />'
+  );
+}
+
+async function handleSearch(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const countries = formData
+    .getAll("country")
+    .map((country) => country.trim())
+    .filter((country) => country);
+
+  try {
+    const capitals = await serviceCountry(countries);
+    const weather = await serviceWeather(capitals);
+
+    refs.list.innerHTML = createMarkup(weather);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    event.target.reset();
+    refs.formContainer.innerHTML = '<input type="text" name="country" />';
+  }
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ temp_c, icon, text, name, country }) => `<li>
+  <img src="${icon}" alt="${text}" />
+  <h2>${country}</h2>
+  <h2>${name}</h2>
+  <p>${text}</p>
+  <p>${temp_c}</p>
+</li>`
+    )
+    .join("");
+}
+
+async function serviceCountry(countries) {
+  const resp = countries.map(async (country) => {
+    const { data } = await axios(
+      `https://restcountries.com/v3.1/name/${country}`
+    );
+    return data[0].capital[0];
+  });
+
+  const data = await Promise.allSettled(resp);
+  return data
+    .filter(({ status }) => status === "fulfilled")
+    .map(({ value }) => value);
+}
+
+async function serviceWeather(capitals) {
+  const BASE_URL = "http://api.weatherapi.com/v1";
+  const API_KEY = "66f9e81543404d02beb160521230808";
+  const END_POINT = "/current.json";
+
+  const resp = capitals.map(async (country) => {
+    const { data } = await axios(
+      `${BASE_URL}${END_POINT}?key=${API_KEY}&q=${country}`
+    );
+    return data;
+  });
+
+  const data = await Promise.allSettled(resp);
+
+  return data
+    .filter(({ status }) => status === "fulfilled")
+    .map(({ value: { current, location } }) => {
+      const {
+        temp_c,
+        condition: { icon, text },
+      } = current;
+      const { name, country } = location;
+      return {
+        temp_c,
+        icon,
+        text,
+        name,
+        country,
+      };
+    });
+}
+
+// const countries = ["Ukraine", "Poland", "Spain", "Canada"];
+// const capitals = ["London", "Warsaw", "Madrid", "Ottawa"];
+
+// serviceWeather(capitals).then(console.log);
